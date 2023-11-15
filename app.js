197248c7167344c4f0b8ed2f3d972b80ae764519
no92one <linus.lindroth.92@gmail.com>
2023-11-15 12:58:56 +0100

    Inital commit

---
+++ app.js
@@ -0,0 +1,10 @@
+window.onresize = screen;
+window.onload = screen;
+
+// Function named 'screen' 👇
+
+function screen() {
+  Width = window.innerWidth;
+  document.getElementById("size").innerHTML
+    = "Width : " + Width + " px"
+}
